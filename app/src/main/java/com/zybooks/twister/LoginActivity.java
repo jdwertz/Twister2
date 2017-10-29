c25852a09dd77c2fc41c6f4cf79c0edada600e9a
thale4 <thale4@harding.edu>
2017-10-29 16:55:08 -0500

    Minor fixes.

--- app/src/main/java/com/zybooks/twister/LoginActivity.java
+++ app/src/main/java/com/zybooks/twister/LoginActivity.java
@@ -1,13 +1,28 @@
 package com.zybooks.twister;
 
+import android.preference.EditTextPreference;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText mInputUsernameText;
+    private String mUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mInputUsernameText = findViewById(R.id.inputUsernameText);
+    }
+
+    protected void onInputUsernameClicked(View view)
+    {
+        mUsername = mInputUsernameText.getText().toString();
+        Log.d("username", mUsername);
     }
 }
